b4546d1092b7a3661f957c99523bdc3695a68a35
Mike Martinson <mike.j.martinson@gmail.com>
2015-02-14 22:04:34 -0800

    add component

--- app/assets/javascripts/components/thing_input.js.jsx
+++ app/assets/javascripts/components/thing_input.js.jsx
@@ -1,6 +1,17 @@
 var ThingInput = React.createClass({
 
   render: function() {
-    return <div />;
+
+    propTypes: {
+      value: React.PropTypes.integer
+    },
+
+    return (
+      <div class="thing-input">
+        <div class="value-button minus-button">-</div>
+        <div class="value-field">{this.props.value}</div>
+        <div class="value-button plus-button">+</div>
+      </div>
+      );
   }
 });
